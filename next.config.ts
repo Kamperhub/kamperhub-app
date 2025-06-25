d92b479c45d3fadc861bb86eef3facda34cc85db
KamperHub <info@kamperhub.com>
2025-06-25 04:55:18 +0000

    i see something was blocked?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      'https://3000-firebase-studio-1748946751962.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
