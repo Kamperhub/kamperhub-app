73312b7413c43b1549a759b96d62b452921534f4
KamperHub <info@kamperhub.com>
2025-07-08 09:39:09 +0000

    increase the entire apps current text-sm to 18px to start with.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,21 @@ const config = {
         '2xl': '1400px',
       },
     },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['1.125rem', { lineHeight: '1.75rem' }], // Changed from 0.875rem to 1.125rem (18px)
+      base: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+    },
     extend: {
       fontFamily: {
         body: ['Alegreya', 'serif'],
