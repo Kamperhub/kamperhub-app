d7d05e6b611b0f146b466b29b0a993c82e16e9be
KamperHub <info@kamperhub.com>
2025-07-22 10:33:43 +0000

    is this an issue? studio-7983448594:~/studio{main}$ ^C studio-7983448594

--- Users/placeholder/Dev/firebase-studio-prototyper/next.config.js
+++ Users/placeholder/Dev/firebase-studio-prototyper/next.config.js
@@ -50,7 +50,7 @@ const nextConfig = {
       // Fonts
       "font-src 'self' https://fonts.gstatic.com",
       // Connections - Allow self, Google APIs, local dev server, Stripe, and Cloud IDE resources, including websockets
-      "connect-src 'self' https://*.googleapis.com wss://*.cloudworkstations.dev https://*.cloudworkstations.dev https://*.stripe.com https://m.stripe.network https://*.cloudworkstations.googleusercontent.com wss://*.cloudworkstations.googleusercontent.com https://vscode-resource.vscode-cdn.net",
+      "connect-src 'self' https://*.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com wss://*.cloudworkstations.dev https://*.cloudworkstations.dev https://*.stripe.com https://m.stripe.network https://*.cloudworkstations.googleusercontent.com wss://*.cloudworkstations.googleusercontent.com https://vscode-resource.vscode-cdn.net",
       // Workers - Allow self, blobs, and Cloud IDE resources
       "worker-src 'self' blob: https://*.cloudworkstations.googleusercontent.com https://vscode-resource.vscode-cdn.net",
       // Frames - Allow self, Google (for Recaptcha/maps), and Stripe (for portal and payment forms)
