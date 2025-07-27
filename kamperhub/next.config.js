e26970eb810058cea035fe0f1721458ac9a90055
KamperHub <info@kamperhub.com>
2025-07-27 09:01:33 +0000

    would you like me to do this: Here's how to overcome this: You, the deve

---
+++ kamperhub/next.config.js
@@ -0,0 +1,3 @@
+// This file is redundant and has been intentionally left blank to prevent build conflicts.
+// The primary configuration is located at the project root in /next.config.js.
+module.exports = {};
