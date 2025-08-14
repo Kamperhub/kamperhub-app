26edec693df49b8597c609ffea78bc543ae8fcb0
KamperHub <info@kamperhub.com>
2025-08-14 00:12:28 +0000

    Your current diagnosis for the 404 error is on the right track in terms

--- src/app/signup/layout.tsx
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and is being replaced by /src/app/(public)/signup/layout.tsx
-// This placeholder will be removed by the file system.
--- src/app/signup/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and is being replaced by /src/app/(public)/signup/page.tsx
-// This placeholder will be removed by the file system.
